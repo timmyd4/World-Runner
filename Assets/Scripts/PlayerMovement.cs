d42d36a6180fddc5fb423047dbfb4171debc616d
Tim <timmywilliams4665@gmail.com>
2025-06-26 13:48:10 -0700

    Changed logic and am liking the way its headed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 10f;
+    public float rotationSpeed = 200f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
-    public float speed = 50f;
-    private Vector3 moveDirection;
-    private Rigidbody rb;
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    public int maxJumps = 2;
+    private int jumpCount = 0;
 
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        // Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        moveDirection = (transform.forward * v + transform.right * h).normalized;
-    }
-    
-    void FixedUpdate()
-    {
-        rb.velocity = moveDirection * speed;
+        if (isGrounded)
+        {
+            jumpCount = 0;
+            if (velocity.y < 0)
+                velocity.y = -2f;
+        }
+
+        // Jump (before movement)
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpCount++;
+        }
+
+        // Input
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        if (h != 0)
+            transform.Rotate(Vector3.up, h * rotationSpeed * Time.deltaTime);
+
+        Vector3 move = transform.forward * v;
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        // Move controller
+        controller.Move((move * speed + velocity) * Time.deltaTime);
     }
 }
